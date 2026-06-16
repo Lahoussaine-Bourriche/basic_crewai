666b2bc6cee2611af78f20c423cc46d48f7b316c
Lahoussaine Bourriche <hessainbourrich@gmail.com>
2026-06-16 11:17:27 +0200

    Refactor agent and task configurations for improved clarity and functionality

--- src/basic_crewai/crew.py
+++ src/basic_crewai/crew.py
@@ -1,8 +1,9 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai_tools import SerperDevTool
 
-
+search_tool = SerperDevTool()
 @CrewBase
 class BasicCrewai():
     """BasicCrewai crew"""
@@ -11,16 +12,24 @@ class BasicCrewai():
     tasks: list[Task]
 
     @agent
-    def researcher(self) -> Agent:
+    def investigator(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['investigator'], # type: ignore[index]
+            verbose=True,
+            tools = [search_tool]
+        )
+
+    @agent
+    def analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config['analyst'], # type: ignore[index]
             verbose=True
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def copywriter(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            config=self.agents_config['copywriter'], # type: ignore[index]
             verbose=True
         )
 
@@ -31,9 +40,15 @@ class BasicCrewai():
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            config=self.tasks_config['analysis_task'], # type: ignore[index]
+            output_file='report.md'
+        )
+    @task
+    def email_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['email_task'], # type: ignore[index]
             output_file='report.md'
         )
 
--- src/basic_crewai/main.py
+++ src/basic_crewai/main.py
@@ -14,8 +14,7 @@ def run():
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'target_company': 'A fictitious or real local real estate agency (e.g., Century 21 Toulouse)',
     }
 
     try:
@@ -29,8 +28,7 @@ def train():
     Train the crew for a given number of iterations.
     """
     inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
+        "target_company": "A fictitious or real local real estate agency (e.g., Century 21 Toulouse)",
     }
     try:
         BasicCrewai().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
@@ -53,8 +51,7 @@ def test():
     Test the crew execution and returns the results.
     """
     inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
+        "target_company": "A fictitious or real local real estate agency (e.g., Century 21 Toulouse)",
     }
 
     try:
@@ -79,8 +76,7 @@ def run_with_trigger():
 
     inputs = {
         "crewai_trigger_payload": trigger_payload,
-        "topic": "",
-        "current_year": ""
+        "target_company": "",
     }
 
     try:
